fb8b3ab4e922cec694b2bf1a152199084dca576f
zhang-jim <zhang230531@gmail.com>
2023-12-22 12:04:18 +0800

    新增 商品功能 前端頁面 資料庫

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //這將自動生成 index, create, store, show, edit, update, destroy 這些動作的路由。
+    Route::resource('product', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
